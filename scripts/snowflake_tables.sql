70484fc5a23d38ccee73fb91f3b529b7c0cc5c06
Maryam Sabahat <96340288+MaryamSabahat@users.noreply.github.com>
2025-04-10 22:24:28 +0500

    fectchig data from S3 then converting from json to tabular format

--- scripts/snowflake_tables.sql
+++ scripts/snowflake_tables.sql
@@ -1,7 +1,7 @@
 -- Create raw table
 create or replace table yelp_businesses (business_text variant);
 
--- Load data from S3
+-- Load data  yelp business file from S3
 COPY INTO yelp_businesses
 FROM 's3://yelp-project01/yelp_academic_dataset_business.json'
 CREDENTIALS = (
@@ -9,6 +9,28 @@ CREDENTIALS = (
     AWS_SECRET_KEY = 'YOUR_SECRET_KEY'
 )
 FILE_FORMAT = (TYPE = 'JSON');
+
+-- Create tabular format table
+create or replace table tbl_yelp_businesses as 
+select business_text:business_id::string as business_id,
+       business_text:name::string as name,
+       business_text:city::string as city,
+       business_text:state::string as state,
+       business_text:review_count::string as review_count,
+       business_text:stars::number as stars,
+       business_text:categories::string as categories
+from yelp_businesses;
+
+-- Load data  yelp reviews file from S3
+create or replace table yelp_reviews (review_text variant);
+
+COPY INTO yelp_reviews
+FROM 's3://yelp-project01/'
+CREDENTIALS = (
+    AWS_KEY_ID = '[REDACTED]',
+    AWS_SECRET_KEY = '[REDACTED]'
+)
+FILE_FORMAT = (TYPE = JSON);
 
 -- Create tabular format table
 create or replace table tbl_yelp_businesses as 
